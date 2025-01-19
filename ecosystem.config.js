01479376a5f54ed2c5e8e6f910c6e1cfbeb298cb
Mauvis Ledford <switchstatement@gmail.com>
2025-01-19 16:45:59 +0000

    add ecosystem.js config

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [{
+    name: "sogni-creatures-api",
+    script: "./index.js",
+    autorestart: true,
+    watch: false,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    restart_delay: 3000,
+    cron_restart: "0 */8 * * *" // Restart every 8 hours
+  }]
+}
